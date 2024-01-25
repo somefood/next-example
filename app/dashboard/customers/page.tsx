1bb51925e9bdd8864c404d1309f61ab8b2e3566d
Seokju Hong <iamseokjuhong@gmail.com>
2024-01-26 00:19:27 +0900

    chapter7: data fetch 하는 법 waterfall 방식은 순차적이기에 기다리는 시간이 걸릴 수 있기에 Promise.all 을 사용하여 병렬적으로 갖고 올 수 있다. 근데 한 데이터 갖고오는 크기가 커지면.. 다 느려질 수도 있겠다고 한다..!

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,12 @@
-export default function Page() {
-    return <p>Customers Page</p>;
-}
+import { Card } from '@/app/ui/dashboard/cards';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { lusitana } from '@/app/ui/fonts';
+
+export default async function Page() {
+    return (
+        <main>
+            <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}></h1>
+        </main>
+    )
+};
